5fbe72d6b55dd8efac697005191bd450e5df9fd9
andrey_mazo <andreymazo@mail.ru>
2023-04-08 13:48:33 +0300

    ридми

--- players.py
+++ players.py
@@ -13,13 +13,13 @@ class Player:
         self.used_words = used_words if used_words else set()
 
     def word_count(self):
-        Вернуть кол-во слов которое было отгадано
+        """Вернуть кол-во слов которое было отгадано"""
 
     def add_word(self, word):
-        добавить к нашему used_words отгаданное слово
+        """добавить к нашему used_words отгаданное слово"""
 
     def word_repeat_yes_or_no(self, word):
-        проверить на повтор отгаданного слова
+        """проверить на повтор отгаданного слова"""
 
     def __repr__(self):
         return f"Игрок с именем {self.name}"
